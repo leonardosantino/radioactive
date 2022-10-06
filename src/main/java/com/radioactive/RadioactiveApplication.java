6b29fc0ac4a1e11ea8f5331535e801b405e72d33
Leonardo Santino <leonardosantino@live.com>
2022-10-05 22:09:01 -0300

    initial commit

---
+++ src/main/java/com/radioactive/RadioactiveApplication.java
@@ -0,0 +1,13 @@
+package com.radioactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/radioactive/RadioactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radioactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
