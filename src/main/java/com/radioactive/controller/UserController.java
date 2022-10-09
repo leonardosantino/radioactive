54987e6ab2e54aec63c8f6931b96ab2ffed77850
Leonardo Santino <leonardosantino@live.com>
2022-10-09 12:36:00 -0300

    feat: event stream

--- src/main/java/com/radioactive/controller/UserController.java
+++ src/main/java/com/radioactive/controller/UserController.java
@@ -4,6 +4,7 @@ import com.radioactive.domain.document.User;
 import com.radioactive.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,19 +17,25 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<User> save (@RequestBody User user) {
+    public Mono<User> save(@RequestBody User user) {
         return service.save(user);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Mono<User> findById (@PathVariable String id) {
+    public Mono<User> findById(@PathVariable String id) {
         return service.findById(id);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public Flux<User> findAll () {
+    public Flux<User> findAll() {
         return service.findAll();
+    }
+
+    @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Flux<User> findAllByEvents() {
+        return  service.findAll();
     }
 }
