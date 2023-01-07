a20ac854d837154c63bfb8173998a246663148e0
Leonardo Santino <79323700+leonardosantino@users.noreply.github.com>
2023-01-07 20:15:24 -0300

    feat: flatMap

--- src/test/java/com/radioactive/flux/FluxTest.java
+++ src/test/java/com/radioactive/flux/FluxTest.java
@@ -1,11 +1,15 @@
 package com.radioactive.flux;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+import reactor.util.function.Tuple3;
 
 @Slf4j
 public class FluxTest {
@@ -83,5 +87,57 @@ public class FluxTest {
                 .expectSubscription()
                 .expectNext("a", "b", "c", "d")
                 .verifyComplete();
+    }
+
+    @Test
+    void flatMapOperator() {
+        Flux<String> fluxA = Flux.just("a", "b");
+
+        Flux<String> fluxB = fluxA.map(s -> s.toUpperCase())
+                .flatMap(s -> this.findByName(s))
+                .log();
+
+        fluxB.subscribe(stringFlux -> log.info(stringFlux.toString()));
+
+        StepVerifier
+                .create(fluxB)
+                .expectSubscription()
+                .expectNext("NameA1", "NameA2", "NameB1", "NameB2")
+                .verifyComplete();
+    }
+
+
+    public Flux<String> findByName(String name) {
+        return name.equals("A") ? Flux.just("NameA1", "NameA2") : Flux.just("NameB1", "NameB2");
+    }
+
+    @Test
+    void zipOperator() {
+        Flux<String> title = Flux.just("Grand Blue", "Baki");
+        Flux<String> studio = Flux.just("Zero-G", "TMS Entertainment");
+        Flux<Integer> episodes = Flux.just(12, 24);
+
+        Flux<Tuple3<String, String, Integer>> tp = Flux.zip(title, studio, episodes);
+
+        Flux<Anime> anime = tp.flatMap(o -> Flux.just(new Anime(o.getT1(), o.getT2(), o.getT3())))
+                .log();
+
+        anime.subscribe(a -> log.info(a.toString()));
+
+        StepVerifier
+                .create(anime)
+                .expectSubscription()
+                .expectNext(
+                        new Anime("Grand Blue", "Zero-G", 12),
+                        new Anime("Baki", "TMS Entertainment", 24)
+                );
+    }
+
+    @AllArgsConstructor
+    @ToString
+    public class Anime {
+        private String title;
+        private String studio;
+        private Integer episodes;
     }
 }
